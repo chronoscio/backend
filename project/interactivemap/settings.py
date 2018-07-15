6bd9e5f7b3b06389e87efa772478efa62c55f094
whirish <lpoflynn@protonmail.ch>
2018-07-15 16:31:00 -0700

    Properly connect django to postgres container

--- project/interactivemap/settings.py
+++ project/interactivemap/settings.py
@@ -78,6 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
