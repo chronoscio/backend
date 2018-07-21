77c83f391b0d3afdc4c760ae66a8372876fabcc0
whirish <lpoflynn@protonmail.ch>
2018-07-21 16:01:21 -0700

    Tests

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from graphene_django.views import GraphQLView
 
-from . import views
+from interactivemap import settings
 
 urlpatterns = [
-    path('', GraphQLView.as_view(graphiql=True), name='graphql'),
+    path('', GraphQLView.as_view(graphiql=settings.DEBUG), name='graphql'),
 ]
