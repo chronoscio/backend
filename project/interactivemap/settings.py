e6927a784926c53b56666d7b108551cdd797a366
Darwin Wu <darwin@berkeley.edu>
2018-10-22 16:56:10 -0700

    Fix settings.py

--- project/interactivemap/settings.py
+++ project/interactivemap/settings.py
@@ -154,13 +154,10 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_WHITELIST = ("localhost:3000", "interactivemap-frontend-*.now.sh")
 
-AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN", "dwaxe.auth0.com")
-API_IDENTIFIER = os.environ.get("API_IDENTIFIER", "https://dwaxe.auth0.com/api/v2/")
-AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID", "iXGypOg8u5cvAhyY7OYxnkGbgNCK9gDT")
-AUTH0_CLIENT_SECRET = os.environ.get(
-    "AUTH0_CLIENT_SECRET",
-    "sQ-vtQ1Ltzy1JHqwH35M20E6tvuZznXytnPWxa8wUmyuCCn04LMLt342ygg9g51u",
-)
+AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN", "chronoscio.auth0.com")
+API_IDENTIFIER = os.environ.get("API_IDENTIFIER", "https://chronoscio.org/")
+AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID", "")
+AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET", "")
 PUBLIC_KEY = None
 JWT_ISSUER = None
 
