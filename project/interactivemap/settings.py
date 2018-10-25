258c44bbad974b0bf15b82395ac98a9a63f4a4b6
whirish <lpoflynn@protonmail.ch>
2018-10-24 19:27:29 -0700

    update db host

--- project/interactivemap/settings.py
+++ project/interactivemap/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "localhost",
+        "HOST": "db",
         "PORT": 5432,
     }
 }
